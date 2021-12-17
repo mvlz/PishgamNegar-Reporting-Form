3a209e76c1ed44e35cde8e29ca0a4b53f84c0f55
Marzieh Valizadeh <sureh1937@gmail.com>
2021-12-17 14:36:11 +0330

    Improve structure and style

--- src/index.js
+++ src/index.js
@@ -1,47 +1,20 @@
-let reportBtnContainer = document.querySelector(".report-button-container");
-
-let btnTogglers = reportBtnContainer.querySelectorAll(".report-input");
-
-console.log(btnTogglers);
-
-// btnTogglers.forEach((btn) => {
-//   btn.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     let tabName = btn.dataset.button;
-//     console.log(tabName);
-//     let formContents = document.querySelector(".form-contents");
-
-//     for (let i = 0; i < formContents.children.length; i++) {
-//       btnTogglers[i].classList.remove("active");
-//       // formContents.children[i].classList.remove("hidden");
-
-//       // if (formContents.children[i].id === tabName) {
-//       //   continue;
-//       // }
-//       // formContents.children[i].classList.add("hidden");
-//       console.log(btnTogglers[i]);
-//     }
-//     e.target.classList.add("active");
-//   });
-// });
-
 function myFunction() {
-  var checkBox = document.getElementById("myCheck");
-  var checkBoxx = document.getElementById("myyCheck");
-  var text = document.getElementById("text");
+  const checkBox = document.getElementById("myCheck");
+  const checkBoxx = document.getElementById("myyCheck");
+  const salesInfSection = document.getElementById("sales-more-inf");
   if (checkBox.checked == true) {
-    text.style.display = "block";
+    salesInfSection.style.display = "flex";
     checkBox.parentElement.classList.add("active");
     checkBoxx.parentElement.classList.remove("active");
     console.log("گزارش فروش");
   } else {
-    text.style.display = "none";
+    salesInfSection.style.display = "none";
     console.log("گزارش تجمیعی");
     checkBoxx.parentElement.classList.add("active");
     checkBox.parentElement.classList.remove("active");
   }
 }
+
 function checkedReportType() {
   const orderRadio = document.getElementById("order");
   const dateRadio = document.getElementById("date");
@@ -68,3 +41,32 @@ function checkedReportType() {
     orderRadio.nextElementSibling.classList.remove("radio-checked");
   }
 }
+const dateRadios = [...document.querySelectorAll(".date-radios input")];
+console.log(dateRadios);
+
+dateRadios.forEach((dateRadio) => {
+  dateRadio.addEventListener("click", () => {
+    for (let i = 0; i < dateRadios.length; i++) {
+      // const element = array[i];
+      dateRadios[i].nextElementSibling.classList.remove("date-checked");
+      if (dateRadios[i].checked) {
+        continue;
+      }
+    }
+    dateRadio.nextElementSibling.classList.add("date-checked");
+  });
+});
+
+const infoBoxes = [...document.querySelectorAll(".info-box input")];
+// console.log(infoBoxes);
+infoBoxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    // if (box.checked) {
+    //   console.log(box);
+    // }
+    console.log(box.nextElementSibling);
+    box.parentElement.parentElement.classList.toggle("info-box-checked");
+    box.nextElementSibling.classList.toggle("date-checked");
+  });
+});
+function checkInfBoxes() {}
