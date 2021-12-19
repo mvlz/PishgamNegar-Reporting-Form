f67d2511766b022568ad532f48bc0318223fd858
Marzieh Valizadeh <sureh1937@gmail.com>
2021-12-20 00:38:14 +0330

    feat: Add number inputs to order type of report state

--- src/index.js
+++ src/index.js
@@ -27,6 +27,12 @@ function checkedReportType() {
     dateRadio.parentElement.parentElement.parentElement.classList.remove(
       "report-type-checked"
     );
+    dateRadio.parentElement.parentElement.nextElementSibling.classList.add(
+      "disable"
+    );
+    orderRadio.parentElement.parentElement.nextElementSibling.classList.remove(
+      "disable"
+    );
     dateRadio.nextElementSibling.classList.remove("radio-checked");
     console.log("order");
   } else {
@@ -38,11 +44,17 @@ function checkedReportType() {
     orderRadio.parentElement.parentElement.parentElement.classList.remove(
       "report-type-checked"
     );
+    orderRadio.parentElement.parentElement.nextElementSibling.classList.add(
+      "disable"
+    );
+    dateRadio.parentElement.parentElement.nextElementSibling.classList.remove(
+      "disable"
+    );
     orderRadio.nextElementSibling.classList.remove("radio-checked");
   }
 }
 const dateRadios = [...document.querySelectorAll(".date-radios input")];
-console.log(dateRadios);
+// console.log(dateRadios);
 
 dateRadios.forEach((dateRadio) => {
   dateRadio.addEventListener("click", () => {
@@ -70,3 +82,40 @@ infoBoxes.forEach((box) => {
   });
 });
 function checkInfBoxes() {}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  const fromInputs = document.querySelectorAll("#from-number input");
+  const toInputs = document.querySelectorAll("#to-number input");
+  function OTPInput(arrey) {
+    for (let i = 0; i < arrey.length; i++) {
+      arrey[i].addEventListener("keydown", function (event) {
+        if (event.key === "Backspace") {
+          arrey[i].value = "";
+          if (i !== 0) arrey[i - 1].focus();
+        } else {
+          if (i === arrey.length - 1 && arrey[i].value !== "") {
+            return true;
+          } else if (event.keyCode > 47 && event.keyCode < 58) {
+            arrey[i].value = event.key;
+            if (i !== arrey.length - 1) arrey[i + 1].focus();
+            event.preventDefault();
+          } else if (event.keyCode > 47 && event.keyCode < 106) {
+            arrey[i].value = event.key;
+            if (i !== arrey.length - 1) arrey[i + 1].focus();
+            event.preventDefault();
+          } else if (event.keyCode > 94 && event.keyCode < 91) {
+            arrey[i].value = String.fromCharCode(event.keyCode);
+            if (i !== arrey.length - 1) arrey[i + 1].focus();
+            event.preventDefault();
+          }
+        }
+      });
+    }
+  }
+  OTPInput(fromInputs);
+  OTPInput(toInputs);
+});
+// window.addEventListener("beforeunload", (event) => {
+//   event.preventDefault();
+//   event.returnValue = "";
+// });
