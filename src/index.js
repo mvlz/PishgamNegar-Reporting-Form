26f5ac2391c2b0ccc0d32d18627d237ad7bb7ac6
Marzieh Valizadeh <sureh1937@gmail.com>
2021-12-22 18:42:18 +0330

    perf: Change some styles code in index.js and add error handling on order inputs

--- src/index.js
+++ src/index.js
@@ -1,65 +1,71 @@
-function myFunction() {
-  const checkBox = document.getElementById("myCheck");
-  const checkBoxx = document.getElementById("myyCheck");
+//Change style on click each elements and show the More information container when Sales one clicked.
+function reportStateCheck() {
+  const salesBtn = document.getElementById("sales");
+  const aggregateBtn = document.getElementById("Aggregate");
   const salesInfSection = document.getElementById("sales-more-inf");
-  if (checkBox.checked == true) {
+  if (salesBtn.checked == true) {
     salesInfSection.style.display = "flex";
-    checkBox.parentElement.classList.add("active");
-    checkBoxx.parentElement.classList.remove("active");
-    console.log("گزارش فروش");
+    salesBtn.parentElement.classList.add("active");
+    aggregateBtn.parentElement.classList.remove("active");
+    // console.log("گزارش فروش");
   } else {
     salesInfSection.style.display = "none";
-    console.log("گزارش تجمیعی");
-    checkBoxx.parentElement.classList.add("active");
-    checkBox.parentElement.classList.remove("active");
+    aggregateBtn.parentElement.classList.add("active");
+    salesBtn.parentElement.classList.remove("active");
+    // console.log("گزارش تجمیعی");
   }
 }
 
-function checkedReportType() {
-  const orderRadio = document.getElementById("order");
-  const dateRadio = document.getElementById("date");
-  console.log(orderRadio.parentElement.parentElement.parentElement);
-  if (orderRadio.checked == true) {
-    orderRadio.nextElementSibling.classList.add("radio-checked");
-    orderRadio.parentElement.parentElement.parentElement.classList.add(
+// Change styles of report type boxes on click.
+const reportTypeRadios = [
+  ...document.querySelectorAll(".report-type-header input"),
+];
+reportTypeRadios.forEach((typeRadio) => {
+  typeRadio.addEventListener("click", () => {
+    let disableInputs = [...document.querySelectorAll(".disable input")];
+    let enableInputs = [
+      ...document.querySelectorAll(".report-type-body input"),
+    ];
+    for (let i = 0; i < reportTypeRadios.length; i++) {
+      reportTypeRadios[i].nextElementSibling.classList.remove("radio-checked");
+      reportTypeRadios[
+        i
+      ].parentElement.parentElement.parentElement.classList.remove(
+        "report-type-checked"
+      );
+      reportTypeRadios[
+        i
+      ].parentElement.parentElement.nextElementSibling.classList.add("disable");
+      console.log(disableInputs);
+
+      enableInputs.forEach((f) => {
+        f.disabled = true;
+        f.value = "";
+        console.log(f.disabled);
+      });
+      if (reportTypeRadios[i].checked) {
+        continue;
+      }
+    }
+    typeRadio.nextElementSibling.classList.add("radio-checked");
+    typeRadio.parentElement.parentElement.parentElement.classList.add(
       "report-type-checked"
     );
-    dateRadio.parentElement.parentElement.parentElement.classList.remove(
-      "report-type-checked"
-    );
-    dateRadio.parentElement.parentElement.nextElementSibling.classList.add(
+    typeRadio.parentElement.parentElement.nextElementSibling.classList.remove(
       "disable"
     );
-    orderRadio.parentElement.parentElement.nextElementSibling.classList.remove(
-      "disable"
-    );
-    dateRadio.nextElementSibling.classList.remove("radio-checked");
-    console.log("order");
-  } else {
-    console.log("date");
-    dateRadio.nextElementSibling.classList.add("radio-checked");
-    dateRadio.parentElement.parentElement.parentElement.classList.add(
-      "report-type-checked"
-    );
-    orderRadio.parentElement.parentElement.parentElement.classList.remove(
-      "report-type-checked"
-    );
-    orderRadio.parentElement.parentElement.nextElementSibling.classList.add(
-      "disable"
-    );
-    dateRadio.parentElement.parentElement.nextElementSibling.classList.remove(
-      "disable"
-    );
-    orderRadio.nextElementSibling.classList.remove("radio-checked");
-  }
-}
+    // console.log(disableInputs);
+    disableInputs.forEach((f) => {
+      f.disabled = false;
+    });
+  });
+});
+
+// change styles of the Time&date-report-type input radios
 const dateRadios = [...document.querySelectorAll(".date-radios input")];
-// console.log(dateRadios);
-
 dateRadios.forEach((dateRadio) => {
   dateRadio.addEventListener("click", () => {
     for (let i = 0; i < dateRadios.length; i++) {
-      // const element = array[i];
       dateRadios[i].nextElementSibling.classList.remove("date-checked");
       if (dateRadios[i].checked) {
         continue;
@@ -69,60 +75,90 @@ dateRadios.forEach((dateRadio) => {
   });
 });
 
+// change styles of checkboxes of payment ways and More information in Sales state
 const infoBoxes = [...document.querySelectorAll(".info-box input")];
-// console.log(infoBoxes);
 infoBoxes.forEach((box) => {
   box.addEventListener("click", () => {
-    // if (box.checked) {
-    //   console.log(box);
-    // }
-    console.log(box.nextElementSibling);
     box.parentElement.parentElement.classList.toggle("info-box-checked");
     box.nextElementSibling.classList.toggle("date-checked");
   });
 });
-function checkInfBoxes() {}
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  const fromInputs = document.querySelectorAll("#from-number input");
-  const toInputs = document.querySelectorAll("#to-number input");
-  function OTPInput(arrey) {
-    for (let i = 0; i < arrey.length; i++) {
-      arrey[i].addEventListener("keydown", function (event) {
-        if (event.key === "Backspace") {
+// document.addEventListener("DOMContentLoaded", function (event) {
+
+// Order type of report logic
+const fromNumInputs = document.querySelectorAll("#from-number input");
+const toNumInputs = document.querySelectorAll("#to-number input");
+const fromNumError = document.querySelector(".from-error");
+const toNumError = document.querySelector(".to-error");
+let fromNumberVal = {
+  num: 1,
+};
+let toNumberVal = {
+  num: 2,
+};
+// order type number input execute and error handling function
+function orderInput(arrey, numError, joinVal) {
+  let x;
+  for (let i = 0; i < arrey.length; i++) {
+    arrey[i].addEventListener("keydown", function (event) {
+      if (event.key === "Backspace") {
+        if (!arrey[i].value == "") {
           arrey[i].value = "";
-          if (i !== 0) arrey[i - 1].focus();
         } else {
-          if (i === arrey.length - 1 && arrey[i].value !== "") {
-            return true;
-          } else if (event.keyCode > 47 && event.keyCode < 58) {
-            arrey[i].value = event.key;
-            if (i !== arrey.length - 1) arrey[i + 1].focus();
-            event.preventDefault();
-          } else if (event.keyCode > 47 && event.keyCode < 106) {
-            arrey[i].value = event.key;
-            if (i !== arrey.length - 1) arrey[i + 1].focus();
-            event.preventDefault();
-          } else if (event.keyCode > 94 && event.keyCode < 91) {
-            arrey[i].value = String.fromCharCode(event.keyCode);
-            if (i !== arrey.length - 1) arrey[i + 1].focus();
-            event.preventDefault();
-          }
+          arrey[i - 1].focus();
         }
-      });
-    }
-  }
-  OTPInput(fromInputs);
-  OTPInput(toInputs);
-});
-// window.addEventListener("beforeunload", (event) => {
-//   event.preventDefault();
-//   event.returnValue = "";
-// });
-const toolBoxes = document.querySelectorAll(".datepicker-container .toolbox");
+      } else {
+        if (i === arrey.length - 1 && arrey[i].value !== "") {
+          return true;
+        } else if (event.keyCode > 47 && event.keyCode < 58) {
+          arrey[i].value = event.key;
+          if (i !== arrey.length - 1) arrey[i + 1].focus();
+          event.preventDefault();
+        } else if (event.keyCode > 95 && event.keyCode < 106) {
+          arrey[i].value = event.key;
+          if (i !== arrey.length - 1) arrey[i + 1].focus();
+          event.preventDefault();
+        } else if (event.keyCode > 94 && event.keyCode < 91) {
+          arrey[i].value = String.fromCharCode(event.keyCode);
+          if (i !== arrey.length - 1) arrey[i + 1].focus();
+          event.preventDefault();
+        }
+      }
 
-// const cancelBtn = document.createElement("button");
-console.log(toolBoxes);
-// // cancelBtn.classList.add("")
-// cancelBtn.innerHTML = "لغو";
-// toolBoxes.appendChild(cancelBtn);
+      // ---- Error handling of order type ----- //
+      x =
+        `${arrey[0].value}` +
+        `${arrey[1].value}` +
+        `${arrey[2].value}` +
+        `${arrey[3].value}` +
+        `${arrey[4].value}`;
+
+      //--Error conditions
+      joinVal.num = parseInt(x);
+      if (x.length < 5) {
+        numError.classList.add("show-error");
+        numError.innerText = "شماره سفارش باید 5 رقمی باشد.";
+      } else if (x.length === 5 && joinVal.num < 87354) {
+        numError.classList.add("show-error");
+        numError.innerText = "شماره سفارش معتبر نیست.";
+      } else if (
+        fromNumberVal.num > 87354 &&
+        toNumberVal.num > 87354 &&
+        fromNumberVal.num > toNumberVal.num
+      ) {
+        console.log("false");
+        toNumError.classList.add("show-error");
+        toNumError.innerText = "شماره سفارش اشتباه است.";
+      } else {
+        numError.classList.remove("show-error");
+        arrey[i].classList.remove("validate");
+      }
+    });
+  }
+}
+// Calling function to execute for From Order one and To Order One
+orderInput(fromNumInputs, fromNumError, fromNumberVal);
+orderInput(toNumInputs, toNumError, toNumberVal);
+
+// });
