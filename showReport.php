54f3eb6506aa0f5de2a2022f36f25bf159883f7d
Marzieh Valizadeh <sureh1937@gmail.com>
2022-01-07 00:02:18 +0330

    Change the path of the files

--- showReport.php
+++ showReport.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pishgam Negar</title>
-    <link rel="stylesheet" href="./style.css">
-    <link href="assets/Styles/gridjs-mermaid.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link href="assets/styles/gridjs-mermaid.min.css" rel="stylesheet" />
 
 </head>
 
 <body class="show-page">
 
     <header class="header-container">
-        <img src="assets/Img/pn-logo.png" alt="" class="logo">
+        <img src="assets/img/pn-logo.png" alt="" class="logo">
         <a href="index.html" class="backBtn">
             بازگشت
             <span class="icon-Arrow---Left-2-1"></span>
